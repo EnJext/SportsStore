7a88ce83ac79f5f066d1873d773d1a898f8e08ef
EnJext <novakvalik630@gmail.com>
2019-10-27 13:25:00 +0200

    Add CRUD Operations for Products

---
+++ SportsStore/Controllers/AdminController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace SportsStore.Controllers
+{
+    public class AdminController: Controller
+    {
+        private IProductRepository products;
+
+        public AdminController(IProductRepository repo) => products = repo;
+
+        public ViewResult Index() => View(products.Products);
+
+        public ViewResult Edit(int ProductID) => View(products.Products.FirstOrDefault(p => p.ProductID == ProductID));
+
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            if(ModelState.IsValid)
+            {
+                products.SaveProduct(product);
+                TempData["message"] = $"{product.Name} has been saved";
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return View(product);
+            }
+        }
+
+        public ViewResult Create() => View("Edit", new Product());
+
+        public IActionResult Delete(int ProductID)
+        {
+            Product deletedProduct = products.DeleteProduct(ProductID);
+            if(deletedProduct != null)
+            {
+                TempData["message"] = $"{deletedProduct.Name} was deleted";
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
--- SportsStore/Models/EFProductRepository.cs
+++ SportsStore/Models/EFProductRepository.cs
@@ -14,5 +14,42 @@ namespace SportsStore.Models
 
         public IQueryable<Product> Products => context.Products;
 
+        public Product DeleteProduct(int ProductID)
+        {
+            Product productToDelete = context.Products.FirstOrDefault(p => p.ProductID == ProductID);
+
+            if(productToDelete != null)
+            {
+                // удаления объекта из бд
+                context.Products.Remove(productToDelete);
+                context.SaveChanges();
+            }
+            return productToDelete;
+        }
+
+        // EF отслеживает только объекты которые сам создал 
+        // так как объект создан системой привязки модели, EF не видит его похожая ситуация была с сохранением order
+        // для order сипользовали - context.Attach() или AttachRange(); ( указывает EF отслежывать переданый обьект)
+
+        // более понятный способ сохранения изменений объекат в бд - изменить отслеживаемый объект с тем же ID
+        public void SaveProduct(Product product)
+        {
+            if(product.ProductID == 0)
+            {
+                context.Add(product);
+            }
+            else
+            {
+                Product ChangeProduct = Products.FirstOrDefault(p => p.ProductID == product.ProductID);
+                if (ChangeProduct != null)
+                {
+                    ChangeProduct.Name = product.Name;
+                    ChangeProduct.Price = product.Price;
+                    ChangeProduct.Description = product.Description;
+                    ChangeProduct.Category = product.Category;
+                }
+            }
+            context.SaveChanges();
+        }
     }
 }
